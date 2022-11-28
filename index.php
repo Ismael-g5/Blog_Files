e25d0500dc0d690cbc3a88d8468dd471d1bd3fcf
Ismael-g5 <ismaelguedessilva@gmail.com>
2022-11-28 12:10:11 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+require 'config.php';
+
+include 'src/Artigo.php';
+$artigo = new Artigo($mysql);
+$artigos = $artigo->exibirTodos();
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Meu Blog</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+    <div id="button" href="style.css">
+    <p>
+    <a class="button" href="/blog/admin" style = " padding: 5px;
+  background-color: #FFF;
+  color: #000;
+  font-weight: bold;
+  border: solid 1px rgb(4, 58, 109);">Ir para a Pagina Administrativa</a>
+    </p>
+
+
+    </div>
+    <div id="container">
+        <h1>Meu Blog</h1>
+        <?php foreach ($artigos as $artigo) : ?>
+        <h2>
+            <a href="artigo.php?id=<?php echo $artigo['id']; ?>">
+                <?php echo $artigo['titulo']; ?>
+            </a>
+        </h2>
+        <p>
+            <?php echo ($artigo['conteudo']); ?>
+        </p>
+        <?php endforeach; ?>
+    </div>
+</body>
+
+</html>
